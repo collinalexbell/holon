ca41ece352482e016b7254218b886b0499069959
slightlycyborg <collinalexbell@gmail.com>
2018-02-23 12:54:43 -0600

    Comments on main.lisp were ugly

--- main.lisp
+++ main.lisp
@@ -5,33 +5,11 @@
 (defpackage :todo
   (:use :timer :cl))
 
-
 (in-package :todo)
 
 (load "todo/globals.lisp")
-
 (load "todo/todo.lisp")
-;;class todo
-
-;;;;TODO-LIST FNS;;;;;
 (load "todo/todo-list.lisp")
-;;complete-todo
-;;sort-by-priority
-;;add-templated-todos
-;;add-todo
-;;load-todos
-;;save-todos
-;;filter-todos-by-group
-;;current-groups
-
-
-;;;;PRINTING;;;;
 (load "todo/printing.lisp")
-;;print-todo-menu
-;;print-current-groups
-;;print-current-groups
-;;print-todo-list
-;;print-current-todo
-
 (load "todo/api.lisp")
 
