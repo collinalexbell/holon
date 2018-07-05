21eccbc18a3a7a2976035bfae3f542dbb70ae62c
slightlycyborg <collinalexbell@gmail.com>
2018-07-05 16:44:01 -0700

    todo has init

--- main.lisp
+++ main.lisp
@@ -9,13 +9,16 @@
 
 (in-package :todo)
 
-(load "alexander.todo/globals.lisp")
-(load "alexander.todo/todo.lisp")
-(load "alexander.todo/todo-list.lisp")
-(load "alexander.todo/printing.lisp")
-(load "alexander.todo/api.lisp")
-
-
 (defun run-all-package-tests ()
   (test 'select-group-test)
   (test 'add-todo-test))
+
+(defun init ()
+  (load "todo/globals.lisp")
+  (load "todo/todo.lisp")
+  (load "todo/todo-list.lisp")
+  (load "todo/printing.lisp")
+  (load "todo/api.lisp")
+  (load "todo/storage.lisp"))
+
+(init)
