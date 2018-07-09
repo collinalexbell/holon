84b982575fd19bd0913e1173249e68b5c23d3b8f
slightlycyborg <collinalexbell@gmail.com>
2018-07-09 07:20:36 -0700

    threaded www

--- hooks.lisp
+++ hooks.lisp
@@ -7,14 +7,17 @@
         do (progn (format t "~a~%" x) (sleep 1)))
   (format t "Snap!!!!!~%"))
 
+(defun www (url)
+  (sb-thread:make-thread (lambda () (trivial-open-browser:open-browser url))))
+
 
 (defun hook-select-WRITE_DOWN_GOALS (the-todo)
-  (trivial-open-browser:open-browser  "https://www.deviantart.com/renny08/art/Avengers-About-Tony-Stark-303404558?offset=0"))
+  (www  "https://www.deviantart.com/renny08/art/Avengers-About-Tony-Stark-303404558?offset=0"))
 
 
 (defun hook-complete-WRITE_DOWN_GOALS (the-todo)
-  (trivial-open-browser:open-browser  "https://www.deviantart.com/anna-kokoro/art/Cyborgs-78509781"))
+  (www  "https://www.deviantart.com/anna-kokoro/art/Cyborgs-78509781"))
 
 
 (defun hook-select-WRITE_SOME_CODE (the-todo)
-  (trivial-open-browser:open-browser "http://www.lispworks.com/documentation/HyperSpec/Front/"))
+  (www "http://www.lispworks.com/documentation/HyperSpec/Front/"))
