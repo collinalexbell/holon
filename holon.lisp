7bb36abec5aa5ddb2a6661e6a008a39d57858059
slightlycyborg <collinalexbell@gmail.com>
2018-12-19 13:48:48 +0000

    klaso holon aldonis

--- holon.lisp
+++ holon.lisp
@@ -4,4 +4,13 @@
 
 (in-package :holon)
 
+
+(defclass holon ()
+  ((name :initarg :name)
+   (inferior-holons :initform '())
+   (superior-holons :initform '())))
+
+(defun new-holon (name)
+  (make-instance 'holon :name name))
+
 (load "task/init.lisp")
