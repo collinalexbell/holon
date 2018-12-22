b6cef370c4c4fa977d3ed9d84c9a29bc45e5e500
slightlycyborg <collinalexbell@gmail.com>
2018-12-22 02:34:27 +0000

    removed common-patterns.lisp

--- common-patterns.lisp
+++
@@ -1,9 +0,0 @@
-(in-package :todo)
-
-(defun add-cleaning-todos ()
-  (let ((cleaning-todos '((make bed)
-			  (pick up desk)
-			  (pick up floor))))
-    (loop for todo in cleaning-todos
-	  do (add-todo todo))
-    (todos)))
