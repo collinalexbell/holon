c52cddaeb5bdff0996a735869e8e6c4a90acffbc
slightlycyborg <collinalexbell@gmail.com>
2018-12-25 15:59:41 +0000

    init-I added

--- i.lisp
+++ i.lisp
@@ -1,3 +1,8 @@
 (in-package :holon)
 
-(defvar I (new-holon 'I))
+(defun init-I ()
+ (defvar I (new-holon 'I))
+  (setf (slot-value I 'superior-holons)
+	(concatenate 'list
+		     '(USA Tennessee Kroger)
+		     (slot-value I 'superior-holons))))
