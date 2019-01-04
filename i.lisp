105426e783496679111516ac3a0718e79244c812
slightlycyborg <collinalexbell@gmail.com>
2019-01-04 15:34:20 +0000

    add inferior-holons to I

--- i.lisp
+++ i.lisp
@@ -1,8 +1,12 @@
 (in-package :holon)
 
 (defun init-I ()
- (defvar I (new-holon 'I))
+  (defvar I (new-holon 'I))
   (setf (slot-value I 'superior-holons)
 	(concatenate 'list
 		     '(USA Tennessee Kroger)
-		     (slot-value I 'superior-holons))))
+		     (slot-value I 'superior-holons)))
+  (setf (slot-value I 'inferior-holons)
+	(concatenate 'list
+		     '(mind body)
+		     (slot-value I 'inferior-holons))))
