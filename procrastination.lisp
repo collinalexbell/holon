b6ce22cde432f9ba5a1ea6fc545bc533c3dac0ef
slightlycyborg <collinalexbell@gmail.com>
2019-01-23 15:34:42 +0000

    procrastination.lisp: +(defun procrastination-here-is-positive? ...)

--- procrastination.lisp
+++ procrastination.lisp
@@ -5,3 +5,8 @@
   (focus human
 	 (set-difference (possible-foci human)
 			 (list task))))
+
+(defun procrastination-here-is-positive? (human task)
+   (or
+    (super-risky? (simulate-task-risk-reward human task))
+    (must-wait-on-information-generation? human task)))
