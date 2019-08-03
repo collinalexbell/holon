d63bce10dcf9dbff32d407679a5fb0019401597a
slightlycyborg <collinalexbell@gmail.com>
2019-08-03 11:38:04 +0000

    Pomodoro runs on seperate thread

--- pomodoro.lisp
+++ pomodoro.lisp
@@ -1,5 +1,7 @@
 (in-package :holon)
 
+(defvar *pomodoro-stream* *standard-output*)
+
 (defun pomodoro (time-in-minutes number-of-tasks)
   (let ((meditate-time (* time-in-minutes 0.05))
         (mastery-time (* time-in-minutes 0.05))
@@ -25,13 +27,18 @@
               (("Take a rest for ~a minutes~%"
                 ,rest-time)
                ,rest-time))))
-     (loop
-       for instruction in instructions
-       do (apply #'msg-sleep-and-ping instruction)))))
+      (sb-thread:make-thread
+       (lambda () (loop
+                    for instruction in instructions
+                    do (apply #'msg-sleep-and-ping instruction)))))))
+
+(defun get-pomodoro-thread ()
+  (find-if (lambda (thread) (equal "pomodoro" (sb-thread:thread-name thread)))
+           (sb-thread:list-all-threads)))
 
 
 (defun msg-sleep-and-ping (msg mins-to-sleep)
-  (apply #'format t msg)
+  (apply #'format *pomodoro-stream* msg)
   (sleep-mins mins-to-sleep)
   (sb-ext:run-program "/usr/bin/aplay" '("resources/r2d2.wav")))
 
