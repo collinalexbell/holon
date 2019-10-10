74011bd39e73eb79257d66646f6acb1032b5ac9c
slightlycyborg <collinalexbell@gmail.com>
2019-10-10 15:39:39 +0000

    HumanRequriedTest added to run tests that need human input

---
+++ src/test/java/holon/utilities/HumanRequiredTest.java
@@ -0,0 +1,17 @@
+package holon.utilities;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import javax.swing.JOptionPane;
+
+public class HumanRequiredTest {
+	public static boolean didItHappen(String thingToHappen) {
+		int reply = JOptionPane.showConfirmDialog(null, "Did "+thingToHappen+"?", "Yes/No",  JOptionPane.YES_NO_OPTION);
+		if (reply == JOptionPane.YES_OPTION)
+		{
+			return true;
+		}
+		return false;
+	}
+}
