8c189a5a7f9f4c585a37146a107d80a43933ea22
slightlycyborg <collinalexbell@gmail.com>
2019-10-16 21:29:54 +0000

    refactor Shell.java

--- src/holon/shell/Shell.java
+++ src/holon/shell/Shell.java
@@ -57,41 +57,48 @@ public class Shell implements Runnable{
 
     }
 
+    private void testClass(String[] command) {
+        try{
+            ClassLoader classLoader = new ShellClassLoader(
+                    classPath, Shell.class.getClassLoader());
+            Class testRunner = classLoader.loadClass("holon.shell.TestRunner");
+
+            Class[] testClasses = new Class[command.length-1];
+            for(int i = 1; i < command.length; i++) {
+                testClasses[i-1] = classLoader.loadClass(command[i]);
+            }
+            if(testClasses.length > 0) {
+                try {
+                    Method runTests = testRunner.getMethod("runTests", Class[].class);
+                    Object result = runTests.invoke(null, new Object[]{testClasses});
+                } catch(Exception e){}
+            }
+        } catch (ClassNotFoundException exception) {}
+    }
+
     private void shellLoop() {
         String[] command = new String[]{};
 
         while(command.length < 1 || !command[0].equals("exit")) {
             showPrompt();
             command = getCommand();
-            for(String str: command){
-            }
 
-            if(command.length < 1) continue;
-            if(command[0].equals("test")) {
-                try{
-                    ClassLoader classLoader = new ShellClassLoader(
-                            classPath, Shell.class.getClassLoader());
-                    Class testRunner = classLoader.loadClass("holon.shell.TestRunner");
-
-                    Class[] testClasses = new Class[command.length-1];
-                    for(int i = 1; i < command.length; i++) {
-                        testClasses[i-1] = classLoader.loadClass(command[i]);
-                    }
-                    if(testClasses.length > 0) {
-                        try {
-                        Method runTests = testRunner.getMethod("runTests", Class[].class);
-                        Object result = runTests.invoke(null, new Object[]{testClasses});
-                        } catch(Exception e){}
-                    }
-                } catch (ClassNotFoundException exception) {}
-            } else {
-                String[] args = {};
-                runJavaProgram(command[0], args);
+            if(command.length > 0) {
+                switch(command[0]) {
+                    case "test":
+                        testClass(command);
+                        break;
+                    default:
+                        runJavaProgram(command);
+                }
             }
         }
     }
 
-    public void runJavaProgram(String packageName, String[] args) {
+    public void runJavaProgram(String[] command) {
+        String[] args = new String[command.length-1];
+        System.arraycopy(command, 1, args, 0, command.length-1);
+        String packageName = command[0];
         ClassLoader classLoader = new ShellClassLoader(
                 classPath, Shell.class.getClassLoader());
 
