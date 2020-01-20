f0894fd56e3d61c5869c913530ab5309f8d553a7
slightlycyborg <collinalexbell@gmail.com>
2020-01-20 22:10:28 +0000

    semicolons for the hinter

--- node/goal.js
+++ node/goal.js
@@ -1,29 +1,28 @@
-var goals = Array()
+var goals = Array();
 
 class Goal {
-  constructor (name) {
-    this.name = name
-    this.id =
-    this.description = ""
-    this.created = new Date()
-    this.completed = false
-  }
+    constructor (name) {
+        this.name = name;
+        this.description = "";
+        this.created = new Date();
+        this.completed = false;
+    }
 
-  create (name) {
-    goal = new Goal(name)
-    goal.save()
-    return goal
-  }
+    create (name) {
+        goal = new Goal(name);
+        goal.save();
+        return goal;
+    }
 
-  static save(goal) {
-    goal.id = goals.length
-    goals.push(goal)
-  }
+    static save(goal) {
+        goal.id = goals.length;
+        goals.push(goal);
+    }
 
-  static load(id) {
-    console.log(goals)
-    return goals[id]
-  }
+    static load(id) {
+        console.log(goals);
+        return goals[id];
+    }
 }
 
-module.exports = Goal
+module.exports = Goal;
