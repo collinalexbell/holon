9991051b8421109bcda745af2120945df8ad2c32
slightlycyborg <collinalexbell@gmail.com>
2021-07-05 15:34:41 +0000

    Using daemon class now

--- lisp/daemons.lisp
+++ lisp/daemons.lisp
@@ -1,47 +1,56 @@
+
+(defclass daemon () ((name :initarg :name) (description :initarg :description)))
+
+(defun print-daemon (daemon)
+  (format t "name: ~a~%description: ~a~%"
+          (slot-value daemon 'name)
+          (slot-value daemon 'description)))
 (defun products ()
- '((sherlock .
-    "Hardware looks like a game controller with a pocket daemon installed")
-   (pocket-daemon .
-    "Handheld daemon gadget terminal composed of an RPi, small touch screen, and lots of USB goodies. Tight Tactical Gear for dense urban environments")
-   (cybook .
-    " A digital book experience ran on a PiZero. To be enjoyed with real book and beer.")
-   (daemon-head .
-    "A medium head for a daemon. Fits RPi Daemons.")
-   (wray .
-    "Platform robot meant for deploying smaller machines")
-   (onix-battlestation .
-    "A colleague daemon battlestation meant for hardcore work (navy seal cybernetics)")
-   (saphira .
-    "A versatile daemon interested in organic structures and the outdoors.")
-   (shelfie .
-    "A daemon with robotic arm for book shelf item manipulation")
-   (gia .
-    "Robot meant for plants")
-   (rod-of-sight-and-signal .
-    "Intelligent signal generator for a modern age")
-   (holonic .
-    "3d representation space of voxels, delivered via Steam")
-   (luxium .
-    "Pixar lamp clone")
-   (steel-raven .
-    "A supercharged rubber ducky w/ RPi zero")
-   (microdaemon .
-    "A RPi Zero powered daemon robot that drives fast as fuck")
-   (daemon-10 .
-    "10th daemon described in the black moleskine notebook from NYC")
-   (kuberdog-sentinel .
-    "Wearable for dogs")
-   (cypack .
-    "Cybernetic backpack")
-   (helm .
-    "HUD for cyborgs")
-   (kuberdog-console .
-    "holonic + pocket-daemon + sherlock for dogs")
-   (plastois .
-    "A 3d printer")
-   (onix .
-    "A professional trainer/mentor in productivity and performace")
-   (onix-bot .
-    "A robotic body for onix")
-   (giactory .
-    "Daemon for farm. To live on Colorado property")))
+  (mapcar
+   (lambda (item) (make-instance 'daemon :name (car item) :description (cdr item)))
+   '((sherlock .
+      "Hardware looks like a game controller with a pocket daemon installed")
+     (pocket-daemon .
+      "Handheld daemon gadget terminal composed of an RPi, small touch screen, and lots of USB goodies. Tight Tactical Gear for dense urban environments")
+     (cybook .
+      " A digital book experience ran on a PiZero. To be enjoyed with real book and beer.")
+     (daemon-head .
+      "A medium head for a daemon. Fits RPi Daemons.")
+     (wray .
+      "Platform robot meant for deploying smaller machines")
+     (onix-battlestation .
+      "A colleague daemon battlestation meant for hardcore work (navy seal cybernetics)")
+     (saphira .
+      "A versatile daemon interested in organic structures and the outdoors.")
+     (shelfie .
+      "A daemon with robotic arm for book shelf item manipulation")
+     (gia .
+      "Robot meant for plants")
+     (rod-of-sight-and-signal .
+      "Intelligent signal generator for a modern age")
+     (holonic .
+      "3d representation space of voxels, delivered via Steam")
+     (luxium .
+      "Pixar lamp clone")
+     (steel-raven .
+      "A supercharged rubber ducky w/ RPi zero")
+     (microdaemon .
+      "A RPi Zero powered daemon robot that drives fast as fuck")
+     (daemon-10 .
+      "10th daemon described in the black moleskine notebook from NYC")
+     (kuberdog-sentinel .
+      "Wearable for dogs")
+     (cypack .
+      "Cybernetic backpack")
+     (helm .
+      "HUD for cyborgs")
+     (kuberdog-console .
+      "holonic + pocket-daemon + sherlock for dogs")
+     (plastois .
+      "A 3d printer")
+     (onix .
+      "A professional trainer/mentor in productivity and performace")
+     (onix-bot .
+      "A robotic body for onix")
+     (giactory .
+      "Daemon for farm. To live on Colorado property"))))
