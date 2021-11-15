658e2f0790074c66b9790696d39d38d928881ebd
kuberlog <collinalexbell@gmail.com>
2021-11-15 15:06:11 +0000

    fix indentation

--- lisp/i.lisp
+++ lisp/i.lisp
@@ -3,12 +3,11 @@
 (defun init-I ()
   (defvar I (new-human 'Collin))
   (setf (slot-value I 'superior-holons)
-	(concatenate 'list
-		     '(USA Tennessee)
-		     (slot-value I 'superior-holons)))
+        (concatenate 'list
+                     '(USA Tennessee)
+                     (slot-value I 'superior-holons)))
   (setf (slot-value I 'inferior-holons)
-	(concatenate 'list
-		     '(mind body)
-		     (slot-value I 'inferior-holons))))
-
+        (concatenate 'list
+                     '(mind body)
+                     (slot-value I 'inferior-holons))))
 (init-I)
