d78e4ba79cb835907b3c284035943c41ad0236e6
kuberlog <collinalexbell@gmail.com>
2022-10-15 04:45:27 +0000

    added say and it works

--- lisp/familiars/familiar.lisp
+++ lisp/familiars/familiar.lisp
@@ -6,3 +6,9 @@
 
 (defun familiar-say-hello (familiar)
   (format t "hello, I am ~s~%" familiar))
+
+(defun familiar-lets-go (familair)
+  (format t "~s, lets go" familair))
+
+(defun say (words)
+  (inferior-shell:run (concatenate 'string "say " words)))
