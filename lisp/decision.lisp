1c6ba2ea547dea75bbc8a6c56be52d96fc9da717
kuberlog <collinalexbell@gmail.com>
2022-10-18 16:02:16 +0000

    global decision file

--- lisp/decision.lisp
+++ lisp/decision.lisp
@@ -1,11 +1,12 @@
 (defpackage :h.decision (:use :cl))
 (in-package :h.decision)
 
-(defparameter global-decisions (with-open-file (decision-file "decisions" :direction :input)
+(defparameter global-decision-file-name "/Users/kuberlog/decisions")
+(defparameter global-decisions (with-open-file (decision-file global-decision-file-name :direction :input)
                                 (read decision-file)))
 
 (defun save ()
-  (with-open-file (decision-file "decisions" :direction :output :if-exists :overwrite)
+  (with-open-file (decision-file global-decision-file-name :direction :output :if-exists :overwrite)
    (write global-decisions :stream decision-file)))
 
 (defun make (description)
