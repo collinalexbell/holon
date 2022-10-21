facf49a77bb8986d1b56eb68a14dccdbccb4e2d6
kuberlog <collinalexbell@gmail.com>
2022-10-21 00:27:49 +0000

    add familiar commands

--- lisp/familiars/familiar.lisp
+++ lisp/familiars/familiar.lisp
@@ -10,5 +10,15 @@
 (defun familiar-lets-go (familair)
   (format nil "~s, lets go" (cadr familair)))
 
+(defun familiar-sit (familiar)
+`(,(cadr familiar) sit))
+
+(defun familiar-come-here (familiar)
+  `(,(cadr familiar) come here))
+
+
 (defun say (words)
-  (inferior-shell:run (concatenate 'string "say " words)))
+  (inferior-shell:run
+   (concatenate 'string "say " (cond
+                                 ((stringp words) words)
+                                 ((listp words) (format nil "~{~a ~}" words))))))
