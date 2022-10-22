77375f1fec5429472b88bdad75608dbf0266bfdd
kuberlog <collinalexbell@gmail.com>
2022-10-22 14:18:28 +0000

    react.js.lisp comment

--- react.js.lisp
+++ react.js.lisp
@@ -2,6 +2,8 @@
 
 (in-package :holon.react)
 
+;; It would be wild if I did all my React programming just typing stuff at a lisp repl. I could splice in transpiled code into my existing codebase. I would keep a meta repo that has all my contributions written in lisp
+
 (defun tag (name)
   (let ((*print-case* :downcase))
     (format t "<~s></~s>" name name)))
