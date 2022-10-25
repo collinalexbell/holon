6c0bc8e7c4081a9878789c48f3860a9bb9562584
kuberlog <collinalexbell@gmail.com>
2022-10-25 00:25:29 +0000

    comment up cybernetics-parts.lisp

--- lisp/cybernetics-parts.lisp
+++ lisp/cybernetics-parts.lisp
@@ -9,8 +9,11 @@
 
 (defmacro def-parts-categories (&rest parts)
   `(progn
+     ;; instead of using lists, maybe I should use holons
      ,@(loop for part in parts collect `(def-parts-category ,(car part) (quote ,(cadr part))))))
 
+
+;; The most imporant part of a macro
 (defvar def-parts-categories-usage
   '(def-parts-categories (spirit (rpi arduino rpi)) (body (motor motor))))
 
