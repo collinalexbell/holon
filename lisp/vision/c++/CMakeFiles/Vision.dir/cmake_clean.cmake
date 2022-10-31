5d568e21fd02d8feca857c1d0a3f3249fdfa0226
kuberlog <collinalexbell@gmail.com>
2022-10-31 04:08:47 +0000

    letsgo

---
+++ lisp/vision/c++/CMakeFiles/Vision.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Vision.dir/vision.cpp.o"
+  "CMakeFiles/Vision.dir/vision.cpp.o.d"
+  "Vision"
+  "Vision.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Vision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
