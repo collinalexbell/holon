beeb4145bbd25b20f90f54ee38a8c7235e963d01
kuberlog <collinalexbell@gmail.com>
2022-10-31 03:20:10 +0000

    comment out create-table so it doesn't overwrite preexisting data, lol, don't connect to prod dbs

--- lisp/interface/psql.lisp
+++ lisp/interface/psql.lisp
@@ -2,15 +2,17 @@
 (ql:quickload :postmodern)
 (use-package :postmodern)
 
-(connect-toplevel "holon" "kuberlog" "" "localhost")
+;(connect-toplevel "holon" "kuberlog" "" "localhost")
+(connect-toplevel "api" "avisemaster" "sD9B62SsyD2ST0qx19Pc" "temporary-db-testing.cko3vpg0edsk.us-east-2.rds.amazonaws.com")
+
 
 (query "select * from users")
 
-(query (:create-table 'files
-                      ((id :type integer :primary-key t :identity-always t)
-                       (name :type string)
-                       (language :type string)
-                       (loc :type integer))))
+;(query (:create-table 'files
+                      ;((id :type integer :primary-key t :identity-always t)
+                      ; (name :type string)
+                      ; (language :type string)
+                      ; (loc :type integer))))
 
 (query "SELECT * from files")
 
@@ -18,8 +20,15 @@
   `(query (:create-table ,name
                          ,body)))
 
+;; here is how it mk-table is used;
 ;(mk-table 'daemons ((name :type string)
 ;                    (purpose :type string)))
+
+;;
+;
+
+;;
+;
 
 ;(query "INSERT INTO files (name, language, loc) VALUES ('psql.lisp', 'lisp', 13)")
 ;(query "UPDATE files SET name='psql.lisp', language= 'lisp' ,loc=18")
