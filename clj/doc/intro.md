628c13056443d2d81f85a5f59f47611acef85c22
kuberlog <collinalexbell@gmail.com>
2022-11-13 08:38:42 +0000

    it is a lisp....but...it is clj

---
+++ clj/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to holon
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clj/src/holon/core.clj
@@ -0,0 +1,12 @@
+(ns holon.core
+  (:gen-class))
+
+(defn im-in
+  "/s"
+  []
+  (println "OMG what a 1337 h4x04 we have on our hands"))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
