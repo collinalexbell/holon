c0378ce3859ac9e4585bb9a194c05f95ccf58162
kuberlog <collinalexbell@gmail.com>
2023-02-19 19:28:31 -0500

    add a description to the holon-book

--- lisp/ecosystem/ecosystem.lisp
+++ lisp/ecosystem/ecosystem.lisp
@@ -10,6 +10,6 @@
     (pictures . ())
     (videos . ())
     (writing . (
-               (holon-book . "https://github.com/kuberlog/holon/blob/master/book/holon.md")
+               (holon-book . ("https://github.com/kuberlog/holon/blob/master/book/holon.md" "A pretty poorly written and embarrasing essay, not really a book")
                ))
     (other-distributables . ())))
