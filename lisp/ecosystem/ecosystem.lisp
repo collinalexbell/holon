8b8b320427574ae4ce19d906a3b2e5fe871fdf3c
kuberlog <collinalexbell@gmail.com>
2023-02-19 19:44:35 -0500

    simple-man robot

--- lisp/ecosystem/ecosystem.lisp
+++ lisp/ecosystem/ecosystem.lisp
@@ -4,12 +4,20 @@
 
 (defun holon-ecosystem ()
   '((code . (
-             (holon . "https://github.com/kuberlog/holon")
-             ))
-    (daemons . ())
+             (holon . ((url . "https://github.com/kuberlog/holon")))
+             (text . ((url . "https://github.com/kuberlog/text")
+                      (description . "a simple text editor")
+                      (usefulness-to-ecosystem . ("learning project"
+                                                  "example of tdd"
+                                                  "example of cli"
+                                                  "minimal usefulness as an actual text editor"))))
+             (choreo . ((url . "https://github.com/kuberlog/choreo")))))
+    (daemons . (
+                (simple-man . ((description . "simplest robot I can think of"))
+                 )))
     (pictures . ())
-    (videos . ())
+    (videos . ((title . "the simplest mobile robot I can think of")))
     (writing . (
                (holon-book . ("https://github.com/kuberlog/holon/blob/master/book/holon.md" "A pretty poorly written and embarrasing essay, not really a book")
-               ))
+                )))
     (other-distributables . ())))
